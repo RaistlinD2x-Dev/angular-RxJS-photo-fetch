86991cd1de1d620a8242ebb1d95e81df11e7eac7
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-28 19:00:08 -0500

    setup API request and authorization

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -7,4 +7,13 @@ import { HttpClient } from '@angular/common/http';
 export class PhotosService {
 
   constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID MSihX20m6ERRMx3v7O4CG0t6WG4uTzCK8E7NGc37j9I'
+      }
+    })
+  }
+
 }
